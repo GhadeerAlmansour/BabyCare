016f01f72777dd482342ce75947b0aba4375fa49
ghaliah99 <114148086+ghaliah99@users.noreply.github.com>
2022-11-07 00:36:52 +0300

    -

--- php/BabySitterSignup.php
+++ php/BabySitterSignup.php
@@ -4,17 +4,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   include '../php/test.php';   
   require_once("Connection.php");
 
-  $con = mysqli_connect('localhost','root','BabyCare');
+  $con = mysqli_connect('localhost','root','','BabyCare');
 
   if(mysqli_connect_errno())
       die("Fail to connect to database :" . mysqli_connect_error());
 
 
- $First_Name = $_POST['first_Name'];
- $Last_Name = $_POST['last_Name'];
- $Email = $_POST['email'];
- $password = $_POST['Password'];
- $city = $_POST['City'];  
+ $First_Name = $_POST['First_Name'];
+ $Last_Name = $_POST['Last_Name'];
+ $Email = $_POST['Email'];
+ $password = $_POST['password'];
+ $city = $_POST['city'];  
  $phone = $_POST['phone'];
  $ID = $_POST['ID'];
  $Age = $_POST['Age'];
@@ -34,24 +34,24 @@ $validateEmail = preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\
 $specialChars = preg_match('@[^\w]@', $password);
 if(!$validateEmail){
     $_SESSION['Sign_up_error'] = "Invalid email address.";
-    header("Location: ../signup.php"); 
-        exit;
+    header("Location: home.php?1");
+    exit;
 }
 if( !$specialChars && strlen($password) < 8) {
     $_SESSION['Sign_up_error'] = 'Password must be at leat 8 characters and contain at least one special character #,&,..';
-    header("Location: ../signup.php"); 
+    header("Location: home.php?2");
     exit;
 }
 if(strlen($password) < 8){
     $_SESSION['Sign_up_error'] = 'Password should be at leat 8 characters!';
-    header("Location: ../signup.php"); 
+    header("Location: home.php?3");
     exit;
 }
 
 
 if( !$specialChars ) {
     $_SESSION['Sign_up_error'] = 'Password must contain at least one special character #,&,..';
-    header("Location: ../signup.php"); 
+    header("Location: home.php?1");
     exit;
 }
 
@@ -63,7 +63,7 @@ $BabySitter_result = mysqli_query($con,$query);
 if (mysqli_num_rows($BabySitter_result)>0)
 {
     $_SESSION['Sign_up_error'] = 'Email exists!';
-    header("Location: ../signup.php"); 
+    header("Location: home.php?4");
     $con -> close();
     exit;
 }
@@ -71,7 +71,7 @@ if (mysqli_num_rows($BabySitter_result)>0)
 
 if($img == null)
 $sql = "INSERT INTO 'Baby_Sitter' (First_Name ,	Last_Name	, Email	, Password	, ID_B	, Age	, Gender, 	City	, 	, Bio)	
-values ('$First_Name', '$Last_Name', '$Email', '$password', '$ID' , '$Age' , '$gender' , '$city' , '$Bio')";else
+values ('$First_Name', '$Last_Name', '$Email', '$password', '$ID' , '$Age' , '$gender' , '$city' , '$Bio')"; 
 else{
   $sql = "INSERT INTO 'Baby_Sitter' (First_Name ,	Last_Name	, Email	, Password	, ID_B	, Age	, Gender, 	City	, Image	, Bio)	
            values ('$First_Name', '$Last_Name', '$Email', '$password', '$ID' , '$Age' , '$gender' , '$city' , ' $img' , '$Bio')";
@@ -80,19 +80,19 @@ else{
 if (mysqli_query($con, $query)) {
     echo "New record created successfully !";
     $_SESSION['email'] = $Email ; //!sure if email
-    header("Location: ../HomeBabySitter.html");
+    header("Location: ../html/HomeBabySitter.html");
     $con -> close();
     exit;
 } else {
     echo "Error: ".mysqli_error($con);
 }
 
-
+/*
  
            values ('$First_Name', '$Last_Name', '$Email', '$password', '$ID' , '$Age' , '$gender' , '$city' , ' $folder' , '$Bio')";
   $result = mysqli_query($conn, $sql);
  
-  $con -> close();
-  
+  $con -> close();*/
+}
 ?>
 
---
+++ php/rejectOffer.php
@@ -0,0 +1,34 @@
+<?php
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "BabyCare";
+echo"HELLLLLO";
+
+$BSoffer_Id = $_POST['rejectedOffer'];
+
+
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+// sql to delete a record
+
+$query = "DELETE FROM offers WHERE BSoffer_Id = 1";
+
+
+if (mysqli_query($conn, $query)) {
+    echo "Record deleted successfully";
+    header("Location: offerList.php?1");
+  } else {
+    echo "Error deleting record: " . mysqli_error($conn);
+    header("Location: offerList.php?2");
+  }
+?>

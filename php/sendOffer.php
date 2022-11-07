b38b3b4b3812dde047793ea5d8828fc30810ccc8
ghaliah99 <114148086+ghaliah99@users.noreply.github.com>
2022-11-07 21:50:41 +0300

    -

--- php/sendOffer.php
+++ php/sendOffer.php
@@ -5,7 +5,7 @@ session_start();
 Define("host","localhost");
 Define("Username", "root");
 Define("Password", "");
-Define("db", "babyare");
+Define("db", "babycare");
          
 $connection = mysqli_connect(host, Username, Password, db);
          
@@ -15,17 +15,21 @@ $connection = mysqli_connect(host, Username, Password, db);
                        $id = $_POST['rowval']; //job request ID that i am sending offer to
                        if(isset($_POST['Price']))
                        $price = $_POST['Price'];
-                       $babysitterEmail = $_SESSION['Email'];
+
+                       $babysitterEmail =  "saraX@outlook.com"; //$_SESSION['Email'];
                       //السطرين احتمال خطا 
-                       $queryN = " SELECT   First_Name , FROM `baby_sitter` WHERE Email= '$babysitterEmail'"; 
-                       $resultN = mysqli_query($connection , $queryN);
+                      $query1 = "SELECT * FROM `Baby_Sitter` WHERE `Email` = '$babysitter' ;";  //to get the babysitter's Info
+                      $result1 = mysqli_query($connection,$query1);
+                      $row1 = mysqli_fetch_array($result1);
+                      $firstName = $row1['First_Name'];
+                      $pending="pending";
                       //
-                       $sql = "INSERT INTO 'offers' (Price, Create_At , Request_Id, Email, , status , BabySitter_Name) VALUES ('$price', now() ,'$id', '$babysitterEmail' ,'pending' , '$resultN')";
+                       $sql = "INSERT INTO `offers` ( Price , Create_At , Request_Id, Email,  status , BabySitter_Name) VALUES ('$price', now() ,'$id', '$babysitterEmail' , '$pending' , '$firstName')";
                        
                        mysqli_query($connection, $sql);
                        if($sql)
                         print(1);
                        $connection -> close();
-                       header("Location: ../jobOffers.php?success=1");
+                       header("Location: jobOffers.php?success=1");
 
            ?>
